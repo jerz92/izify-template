f19c5ffbf6b69d9409e5f1e6da9403d527a6b7e0
nurdin <lowyat_net@yahoo.com>
2013-12-18 11:52:11 +0800

    clean up code

--- js/views/home/HomeView.js
+++ js/views/home/HomeView.js
@@ -1,23 +1,18 @@
-define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'text!templates/home/homeTemplate.html'
-], function($, _, Backbone, homeTemplate){
-
-  var HomeView = Backbone.View.extend({
-    el: $("#page"),
-	
-    render: function(){
-      this.$el.html(homeTemplate);
-	  
-	  	$('[data-toggle=offcanvas]').click(function() {
-    		$('.row-offcanvas').toggleClass('active');
-  		});
-    }
-
-  });
-
-  return HomeView;
-  
+define(['jquery', 'underscore', 'backbone', 'text!templates/home/homeTemplate.html'], function($, _, Backbone, homeTemplate) {
+    var HomeView = Backbone.View.extend({
+        el: $("#page"),
+        initialize: function() {
+            this.$el.off();
+        },
+        events: {
+            'click [data-toggle=offcanvas]': 'toggleClass'
+        },
+        toggleClass: function(e) {
+            this.$('.row-offcanvas').toggleClass('active');
+        },
+        render: function() {
+            this.$el.html(homeTemplate);
+        },
+    });
+    return HomeView;
 });
